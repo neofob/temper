82affff12fd0c144da5061b1e7937425fb385296
Tuan T. Pham <tuan@vt.edu>
2023-12-16 23:59:58 -0500

    Tidy up temper service
    temper-service.py:
        * Tidy up service code with Mixtral 8x7B Instruct 6b model help
    
    Signed-off-by: Tuan T. Pham <tuan@vt.edu>

--- temper-service.py
+++ temper-service.py
@@ -1,51 +1,42 @@
 #!/usr/bin/env python3
 
 import argparse
-import json
+from flask import Flask, json
 
-from flask import Flask
+import temper
 
-from temper import Temper, USBList
+usblist = temper.USBList()
+
+def update_usb_devices(t):
+
+    try:
+        t.usb_devices = usb_list.get_usb_devices()
+    except Exception as e:
+        print(f"Error updating USB devices: {e}")
 
 # parsing config
 parser = argparse.ArgumentParser()
 parser.add_argument("-H", "--host", default="0.0.0.0", help="host to bind to, default: 0.0.0.0")
-parser.add_argument("-p", "--port", default=2610, help="port to listen to, default: 2610")
-parser.add_argument("-d", "--debug", default=False, help="debug mode, default: False")
-
+parser.add_argument("-p", "--port", type=int, default=2610, help="port to listen to, default: 2610")
+parser.add_argument("-d", "--debug", action='store_true', help="debug mode, default: False")
 args = parser.parse_args()
 
-host = args.host
-port = int(args.port)
-debug = args.debug
+app = Flask("temper")
+t = temper.Temper()
 
-print(f"host = {host}")
-print(f"port = {port}")
-print(f"debug = {debug}")
 print(f"Available endpoints:")
 print(f"/list = list available USB devices")
 print(f"/metrics = return availale metrics from temper USB devices")
 
-app = Flask("temper")
-t = Temper()
-
 @app.route('/list')
 def list():
-    # re-read USB list in case we have new plugged in device(s)
-    usblist = USBList()
-    t.usb_devices = usblist.get_usb_devices()
-
-    result = json.dumps(t.usb_devices, indent=2, sort_keys=True)
-    return result
+    update_usb_devices(t)
+    return json.dumps(t.usb_devices, indent=2, sort_keys=True)
 
 @app.route('/metrics')
 def metrics():
-    # re-read USB list in case we have new plugged in device(s)
-    usblist = USBList()
-    t.usb_devices = usblist.get_usb_devices()
-
-    result = json.dumps(t.read(), indent=2)
-    return result
+    update_usb_devices(t)
+    return json.dumps(t.read(), indent=2)
 
 if __name__ == '__main__':
-    app.run(host=host, port=port, debug=debug)
+    app.run(host=args.host, port=args.port, debug=args.debug)
